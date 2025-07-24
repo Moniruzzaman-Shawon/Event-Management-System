df709d7d77ce6cc27cb055ee39ca64d90dcef8b2
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-07-24 12:30:22 +0600

    forms bug fixed

--- users/forms.py
+++ users/forms.py
@@ -43,6 +43,7 @@ class CustomUserCreationForm(UserCreationForm):
         ]
 
 class CustomUserChangeForm(UserChangeForm):
+    password = None
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'profile_picture')
